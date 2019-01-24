76b98791fcad1b2aeba6cf783dc9fb865b36a977
SkyLightSK <kopel@aport.ru>
2019-01-25 00:02:22 +0200

    Add base form settings

---
+++ src/Union/Setting/Elements/FormElement.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Union\Setting\Elements;
+
+abstract class FormElement
+{
+    /**
+     * We can anticipate that all DOM elements require these 3 fields.
+     */
+    protected $name;
+    protected $title;
+    protected $data;
+
+    public function __construct(string $name, string $title)
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setData($data): void
+    {
+        $this->data = $data;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Each concrete DOM element must provide its rendering implementation, but
+     * we can safely assume that all of them are returning strings.
+     */
+    abstract public function render();
+}
